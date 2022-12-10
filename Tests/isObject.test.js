71966df2c7f0b2e422a54534e300f3490953a42f
joona <joona@debian-BULLSEYE-live-builder-AMD64>
2022-12-10 19:59:55 +0200

    isObject unit tests implemented

--- Tests/isObject.test.js
+++ Tests/isObject.test.js
@@ -1,11 +1,27 @@
-test('Basic test1', () => {
-    expect(true);
+import isObject from "../src/isObject.js";
+
+// Positive testing
+
+test('Object should be of type Object', () => {
+  expect(isObject({})).toEqual(true);
+});
+
+test('Vector should not be of type Object', () => {
+    expect(isObject([1, 2, 3])).toEqual(true);
   });
-  
-  test('Basic test2', () => {
-      expect(true);
+
+  test('Function should not be of type Object', () => {
+    expect(isObject(Function)).toEqual(true);
   });
-  
-  test('Basic test3', () => {
-      expect(true);
+
+  test('Null should not be of type Object', () => {
+    expect(isObject(null)).toEqual(false);
+  });
+
+  test('String should not be of type Object', () => {
+    expect(isObject("asdf")).toEqual(false);
+  });
+
+  test('Number should not be of type Object', () => {
+    expect(isObject(1)).toEqual(false);
   });
