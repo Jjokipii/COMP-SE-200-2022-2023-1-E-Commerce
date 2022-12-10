c0194204ef0d93b71be27b08b00dc1b46ecd292e
joona <joona@debian-BULLSEYE-live-builder-AMD64>
2022-12-10 20:22:49 +0200

    slice unit tests added

--- Tests/slice.test.js
+++ Tests/slice.test.js
@@ -1,11 +1,27 @@
-test('Basic test1', () => {
-    expect(true);
+import add from "../src/add.js";
+
+var array = [1, 2, 3, 4]
+
+// Positive testing
+
+test('Slicing 3-END from [1, 2, 3, 4] should output [3, 4]', () => {
+  expect(array.slice(array, 2)).toEqual([3, 4]);
+});
+
+test('Slicing 2-3 from [1, 2, 3, 4] should output [2, 3]', () => {
+    expect(array.slice(array, 1, 2)).toEqual([2, 3]);
+  });
+
+// Negative testing
+
+test('Incorrect parameter 1 returns TypeError', () => {
+    expect(array.slice(null, 2, 3)).toThrowError(TypeError);
   });
   
-  test('Basic test2', () => {
-      expect(true);
+  test('Incorrect parameter 2 returns TypeError', () => {
+    expect(array.slice(array, null, 3)).toThrowError(TypeError);
   });
   
-  test('Basic test3', () => {
-      expect(true);
+  test('Incorrect parameter 1 returns TypeError', () => {
+    expect(array.slice(array, 2, "a")).toThrowError(TypeError);
   });
