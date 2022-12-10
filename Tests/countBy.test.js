fdeaea4e6c90afcfd7632a1ad598a53d25ae1cf7
joona <joona@debian-BULLSEYE-live-builder-AMD64>
2022-12-10 19:01:13 +0200

    countBy units tests added

--- Tests/countBy.test.js
+++ Tests/countBy.test.js
@@ -1,11 +1,25 @@
-test('Basic test1', () => {
-    expect(true);
-  });
+import countBy from "../src/countBy.js";
+
+const users = [
+    { 'user': 'barney', 'active': true },
+    { 'user': 'betty', 'active': true },
+    { 'user': 'fred', 'active': false }
+]
+
+test('Counting active values from users should give { true: 2, false: 1 }', () => {
+    expect(countBy(users, value => value.active)).toEqual({ 'true': 2, 'false': 1 });
+});
+
+// Negative testing
+
+test('Incorrect parameter 1 returns TypeError', () => {
+    expect(countBy("a", 1.111)).toThrowError(TypeError);
+});
   
-  test('Basic test2', () => {
-      expect(true);
-  });
+test('Incorrect parameter 2 returns TypeError', () => {
+    expect(countBy(1.111, "a")).toThrowError(TypeError);
+});
   
-  test('Basic test3', () => {
-      expect(true);
-  });
+test('Incorrect parameter 3 returns TypeError', () => {
+    expect(countBy(1.111, "a")).toThrowError(TypeError);
+});
