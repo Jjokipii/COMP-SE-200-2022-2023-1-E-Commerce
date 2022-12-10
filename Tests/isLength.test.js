0a51aa688cd54ef2cc6fb690adcf9561eb048ba3
joona <joona@debian-BULLSEYE-live-builder-AMD64>
2022-12-10 19:50:13 +0200

    isLenght unit tests added

--- Tests/isLength.test.js
+++ Tests/isLength.test.js
@@ -1,11 +1,23 @@
-test('Basic test1', () => {
-    expect(true);
-  });
+import isLength from "../src/isLength.js";
+
+// Positive testing
+
+test('Test if number is of lenght', () => {
+    expect(isLength(3)).toEqual(true);
+});
   
-  test('Basic test2', () => {
-      expect(true);
-  });
-  
-  test('Basic test3', () => {
-      expect(true);
-  });
+test('Test if number.min is of lenght', () => {
+    expect(isLength(Number.MIN_VALUE)).toEqual(false);
+});
+
+test('Test if number.inf is of lenght', () => {
+    expect(isLength(Infinity)).toEqual(false);
+});
+
+test('Test if char is of lenght', () => {
+    expect(isLength('3')).toEqual(false);
+});
+
+test('Test if string is of lenght', () => {
+    expect(isLength("asdf")).toEqual(false);
+});
